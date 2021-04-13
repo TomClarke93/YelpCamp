16ad9cdcef2e26da6febf4f17c367d6891cb23a6
Tom Clarke <thomas.clarke09@gmail.com>
2021-04-13 22:50:49 +0100

    view route created for campgrounds

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const Campground = require('./models/campground');
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -20,6 +21,11 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('home');
+})
+
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds});
 })
 
 app.listen(3000, () => {
