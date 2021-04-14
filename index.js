1471e2b7dbf71c6c93cfab2b8dd6f8dc78ba968b
Tom Clarke <thomas.clarke09@gmail.com>
2021-04-14 17:15:15 +0100

    new template & route setup. create route setup

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ db.once("open", () => {
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -26,6 +27,16 @@ app.get('/', (req, res) => {
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', {campgrounds});
+})
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const newCampground = new Campground(req.body.campground);
+    await newCampground.save();
+    res.redirect(`/campgrounds/${newCampground._id}`);
 })
 
 app.get('/campgrounds/:id', async (req, res) => {
