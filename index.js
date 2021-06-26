05c32b96148c423f5bb37d9e3d8792375047329e
Tom Clarke <thomas.clarke09@gmail.com>
2021-06-26 17:23:54 +0100

    register functionality"

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews')
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews')
+const userRoutes = require('./routes/users');
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -74,8 +75,10 @@ app.get('/fakeUser', async (req, res) => {
     res.send(newUser);
 });
 
-app.use("/campgrounds", campgrounds);
-app.use("/campgrounds/:id/reviews", reviews);
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
+
 
 
 app.all('*', (req, res, next) => {
