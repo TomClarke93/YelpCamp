413727dd0a513ef19b5271b69196c1c7cc08d135
Tom Clarke <thomas.clarke09@gmail.com>
2021-06-27 11:39:34 +0100

    ux authentication

--- routes/users.js
+++ routes/users.js
@@ -8,11 +8,16 @@ router.get('/register', (req, res) => {
     res.render('users/register')
 });
 
-router.post('/register', catchAsync(async (req, res) => {
+router.post('/register', catchAsync(async (req, res, next) => {
     try {
         const {email, username, password} = req.body
         const user = new User({email, username});
         const registeredUser = await User.register(user, password);
+        req.login(registeredUser, err => {
+            if(err) {
+                return next();
+            }
+        });
         req.flash('success', 'Welcome to YelpCamp!');
         res.redirect('/campgrounds');
     } catch(e) {
@@ -27,7 +32,9 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req, res) => {
         req.flash('success', 'Welcome Back')
-        res.redirect('/campgrounds');
+        const redirectUrl = req.session.returnTo || '/campgrounds';
+        delete req.session.returnTo;
+        res.redirect(redirectUrl);
 });
 
 router.get('/logout', (req, res) => {
